3cbb18b1804c049c1593d0e0f70238bf40cc9b87
David Redmin <david.redmin@trio.dhs.gov>
2021-12-30 11:08:08 -0500

    Satisfy isort pre-commit hook

--- src/mdyml/__init__.py
+++ src/mdyml/__init__.py
@@ -2,11 +2,11 @@
 # Standard Python Libraries
 import re
 
+# cisagov Libraries
 # We disable a Flake8 check for "Module imported but unused (F401)" here because
 # although this import is not directly used, it populates the value
 # package_name.__version__, which is used to get version information about this
 # Python package.
-# cisagov Libraries
 from _log4j_md_yml_version import __version__  # noqa: F401
 
 DEFAULT_CVE_ID = "cve-2021-44228"
